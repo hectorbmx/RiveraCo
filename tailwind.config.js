97d849fe96a294d0a8d277202f83ce50c8aaf673
hector <hectorbmx@gmail.com>
2026-02-09 10:41:12 -0600

    fix(tailwind): include build assets for production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -7,8 +7,20 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+          // ✅ agrega esto
+    './resources/js/**/*.js',
+    './resources/js/**/*.ts',
     ],
-
+  safelist: [
+    // ✅ lo que estás usando en ese botón
+    'bg-rose-600',
+    'hover:bg-rose-700',
+    'text-white',
+    'px-3',
+    'py-1.5',
+    'rounded-lg',
+    'text-xs',
+  ],
     theme: {
         extend: {
             fontFamily: {
