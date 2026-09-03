cffd5dedb9e96efe619f123190c3686dbf60abe8
hector <hectorbmx@gmail.com>
2026-09-03 11:20:18 -0600

    ajust tabla factura

--- resources/views/sat/facturacion/index.blade.php
+++ resources/views/sat/facturacion/index.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Facturación SAT')
 
 @section('content')
-<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
+<div class="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
 
     <div class="flex flex-col gap-3 mb-3 lg:flex-row lg:items-center lg:justify-between">
         <div>
